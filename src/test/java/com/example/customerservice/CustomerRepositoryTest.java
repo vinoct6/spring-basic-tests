1b04528a5d7c8b5e526d52e2dfcb37de7c0b8421
Vinoth kumar <vinoct6@gmail.com>
2017-11-07 10:28:29 +0530

    Explore @Rule annotation to test ConstraintViolation

--- src/test/java/com/example/customerservice/CustomerRepositoryTest.java
+++ src/test/java/com/example/customerservice/CustomerRepositoryTest.java
@@ -2,7 +2,9 @@ package com.example.customerservice;
 
 
 import org.assertj.core.api.BDDAssertions;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,12 +12,18 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.ConstraintViolationException;
+
 import static org.assertj.core.api.BDDAssertions.then;
 
 @SpringBootTest(classes = CustomerServiceApplication.class)
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class CustomerRepositoryTest {
+
+    //https://stackoverflow.com/questions/13489388/how-does-junit-rule-work
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     private TestEntityManager testEntityManager;
@@ -38,6 +46,12 @@ public class CustomerRepositoryTest {
         Customer saved = customerRepository.save(customer);
         then(saved.getId()).isNotNull();
         then(saved.getFirstName()).isEqualTo("firstName");
+    }
+
+    @Test
+    public void newInstanceWithInvalidParametersShouldResultInConstraintViolation() {
+        expectedException.expect(ConstraintViolationException.class);
+        customerRepository.save(new Customer(null, null, null, null));
     }
 
 
